115c3638a2e5a7a67d63c98532725d27e3dc271a
Olexandra Korol <korol.olexandra@gmail.com>
2024-05-20 14:16:31 +0300

    base logic and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+const todos = [
+  {id: 1, title: "test1", complited: false},
+  {id: 2, title: "test2", complited: false},
+  {id: 3, title: "test3", complited: true},
+]
+
+interface TodoProps {
+  id: number,
+  title: string,
+  complited: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-todo-app';
+  editing = false;
+  todos = todos;
+
+  handleTodoToggle(event: Event, todo: TodoProps) {
+    todo.complited = (event.target as HTMLInputElement).checked
+  }
 }
